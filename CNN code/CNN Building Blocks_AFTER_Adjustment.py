3525fb7b9fc71bb9f0fcf2cd439ff483caa03d11
Xalllle <alex.heinle@outlook.com>
2025-03-17 22:41:08 -0400

    Better than random

--- CNN code/CNN Building Blocks_AFTER_Adjustment.py
+++ CNN code/CNN Building Blocks_AFTER_Adjustment.py
@@ -17,31 +17,28 @@ class neuron():
     def __init__(self, weight, bias):
         self.weight = weight
         self.bias = bias
+    def reshape_matrix(self, size):
+        matrix = []
+        for i in range(size):
+            row = self.weight[i * size : (i + 1) * size]
+            matrix.append(row)
+        self.weight = matrix
         
+def convolve(input_image, weight_matrix, bias):
+    size_a = len(input_image)
+    size_b = len(weight_matrix)
 
-        
-        
-def convolve(input_image, weight_matrix, bias, filter_matrix):
-    #Convolves two matrices
-    square_iterator_size = len(input_image) - (len(filter_matrix) - 1)
-    feature_map = [[0 for _ in range(square_iterator_size)] for _ in range(square_iterator_size)]
-    i = 0
-    while i < square_iterator_size:
-        j = 0
-        while j < square_iterator_size:
-            sum_number = 0
-            x = 0
-            while x < len(filter_matrix):
-                y = 0
-                while y < len(filter_matrix[0]):
-                    sum_number = (filter_matrix[x][y] * weight_matrix[x][y] * input_image[x+i][y+j]) + bias
-                    feature_map[i][j] += sum_number 
-                    y += 1
-                
-                x += 1
-                sum_number = 0
-            j += 1
-        i += 1
+    result_size = size_a - size_b + 1
+
+    feature_map = [[0.0 for _ in range(result_size)] for _ in range(result_size)]
+
+    for i in range(result_size):
+        for j in range(result_size):
+            for m in range(size_b):
+                for n in range(size_b):
+                    feature_map[i][j] += float(input_image[i + m][j + n]) * float(weight_matrix[m][n])
+            feature_map[i][j] += float(bias) # Ensure bias is also a float
+
     return feature_map
 
 def relu(feature_map):
@@ -68,21 +65,60 @@ def flatten(feature_maps_list):
                 output_matrix.append(n)
     return output_matrix
 
-def max_pooling(feature_map):
-    size = math.floor(len(feature_map[0])/2)
-    output_feature_map = [[0 for _ in range(size)] for _ in range(size)]
-    i = 0
-    while i < size:
-        j = 0
-        while j < size:
+def fully_connected(flattened_layer, weights, bias):
+    activation = 0
+    a = 0
+    while a < len(flattened_layer):
+        activation += flattened_layer[a] * weights[a]
+        a += 1
+    activation += bias
+    return activation
 
-            output_feature_map[i][j] = max([feature_map[i][j],feature_map[2*i+1][2*j],feature_map[2*i][2*j+1],feature_map[2*i+1][2*j+1]])
-            j += 1
-        i += 1
-    return output_feature_map
+def max_pooling(input_matrix):
+  input_size = len(input_matrix)
+  output_size = input_size // 2
+  output_matrix = [[0 for _ in range(output_size)] for _ in range(output_size)]
+  for i in range(output_size):
+    for j in range(output_size):
+      window = [[input_matrix[2 * i][2 * j], input_matrix[2 * i][2 * j + 1]], [input_matrix[2 * i + 1][2 * j], input_matrix[2 * i + 1][2 * j + 1]]]
+      max_val = window[0][0]
+      for row in window:
+        for val in row:
+          if val > max_val:
+            max_val = val
+      output_matrix[i][j] = max_val
+  return output_matrix
     
 
+def normalize(in_matrix):
+    #Divides all of the numbers in a matrix by the highest number in the matrix
+    max_value = 0
+    for sublist in in_matrix:
+        if max(sublist) > max_value:
+            max_value = max(sublist)
+    out_matrix = [[0 for _ in range(len(in_matrix[0]))] for _ in range(len(in_matrix))]
+    i=0
+    j=0
+    while i < len(in_matrix):
+        while j < len(in_matrix[i]):
+            if max_value != 0:
+                out_matrix[i][j] = round(in_matrix[i][j]/max_value,2)
+            else:
+                out_matrix[i][j] = 0
+            
+            j += 1
+    
+        j = 0
+        i += 1
+    return out_matrix
 
+def softmax(x):
+  exp_x = [2.71828**val for val in x]
+  sum_exp_x = sum(exp_x)
+  return [val / sum_exp_x for val in exp_x]
+
+def sigmoid_list(x):
+  return [1 / (1 + math.exp(-val)) for val in x]
 
 def sigmoid(x):
     if type(x) == list:
@@ -95,13 +131,19 @@ def sigmoid(x):
         return result
     elif type(x) == float:
         return round((1 / (1 + math.exp(-x))),2)
+    
+def check_prediction(output, target_number):
+  if output[0] > output[1]:
+    predicted_number = 0
+  else:
+    predicted_number = 1
+
+  if predicted_number == target_number:
+    return 1
+  else:
+    return 0
 
 if __name__== "__main__":
-    start_time = time.time()
-    
-    #mnist_list = mnist_data_interpreter.read('mnist_train.csv')
-    #input_image = normalize([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 51, 159, 253, 159, 50, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 238, 252, 252, 252, 237, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 54, 227, 253, 252, 239, 233, 252, 57, 6, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 60, 224, 252, 253, 252, 202, 84, 252, 253, 122, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 163, 252, 252, 252, 253, 252, 252, 96, 189, 253, 167, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 51, 238, 253, 253, 190, 114, 253, 228, 47, 79, 255, 168, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 238, 252, 252, 179, 12, 75, 121, 21, 0, 0, 253, 243, 50, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 38, 165, 253, 233, 208, 84, 0, 0, 0, 0, 0, 0, 253, 252, 165, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 7, 178, 252, 240, 71, 19, 28, 0, 0, 0, 0, 0, 0, 253, 252, 195, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 57, 252, 252, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0, 253, 252, 195, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 198, 253, 190, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 253, 196, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 76, 246, 252, 112, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 253, 252, 148, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 85, 252, 230, 25, 0, 0, 0, 0, 0, 0, 0, 0, 7, 135, 253, 186, 12, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 85, 252, 223, 0, 0, 0, 0, 0, 0, 0, 0, 7, 131, 252, 225, 71, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 85, 252, 145, 0, 0, 0, 0, 0, 0, 0, 48, 165, 252, 173, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 86, 253, 225, 0, 0, 0, 0, 0, 0, 114, 238, 253, 162, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 85, 252, 249, 146, 48, 29, 85, 178, 225, 253, 223, 167, 56, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 85, 252, 252, 252, 229, 215, 252, 252, 252, 196, 130, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 28, 199, 252, 252, 253, 252, 252, 233, 145, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 25, 128, 252, 253, 252, 141, 37, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-    
     '''
     Initialize trained values
     '''
@@ -113,35 +155,95 @@ if __name__== "__main__":
     
     output_layer_3_bias = read_file('train_data\output_layer_3_bias.txt')
     output_layer_3_weight = read_file('train_data\output_layer_3_weight.txt')
-
-
-    '''
-    Initialize convolutional neurons
-    '''
-    convolutional_neurons = []
-    i = 0
-    while i < 5:
-        convolutional_neurons.append(neuron(conv_layer_1_weight[9*i:9+9*i], conv_layer_1_bias[i]))
-        print(conv_layer_1_weight[9*i:9+9*i], conv_layer_1_bias[i])
-        i += 1
-
-
-
-
+    
+    mnist_list = mnist_data_interpreter.read('mnist_train.csv')
+    
+    
+    start_time = time.time()
+    iteration = 0
+    correct_number = 0
+    for number in mnist_list:
+        target_number = number[0]
+        input_image = normalize(number[1])
+        
+    
+        '''
+        Convolutional Layer (5 kernels)
+        '''
+        convolutional_neurons = []
+        i = 0
+        while i < 5:
+            convolutional_neurons.append(neuron(conv_layer_1_weight[9*i:9+9*i], conv_layer_1_bias[i]))
+            convolutional_neurons[i].reshape_matrix(3)
+            i += 1
+    
+        convolved_matrices = []
+        i = 0
+        while i < 5:
+            convolved_matrices.append(relu(convolve(input_image, convolutional_neurons[i].weight, convolutional_neurons[i].bias)))
+            i += 1
+    
+        """
+        Pooling Layer
+        """
+    
+        pooled_matrices = []
+        i = 0
+        while i < 5:
+            pooled_matrices.append(max_pooling(convolved_matrices[i]))
+            i += 1
+    
+    
+        """
+        Flattening Layer
+        """
+    
+        flattened_layer = flatten(pooled_matrices)
+        
+        """
+        Fully Connected Layer
+        """
+        
+        dense_layer_2_neurons = []
+        i = 0
+        while i < 10:
+            dense_layer_2_neurons.append(neuron(dense_layer_2_weight[845*i:845+845*i], dense_layer_2_bias[i]))
+            i += 1
+        
+        fully_connected_layer = []
+        i = 0
+        while i < 10:
+            fully_connected_layer.append(max(0,fully_connected(flattened_layer, dense_layer_2_neurons[i].weight, dense_layer_2_neurons[i].bias)))
+            i += 1
+         
+        """
+        Output Layer
+        """
+        output_layer_3_neurons = []
+        i = 0
+        while i < 2:
+            output_layer_3_neurons.append(neuron(output_layer_3_weight[10*i:10+10*i], output_layer_3_bias[i]))
+            i += 1
+        
+        output_layer = []
+        i = 0
+        while i < 2:
+            output_layer.append(fully_connected(fully_connected_layer, output_layer_3_neurons[i].weight, output_layer_3_neurons[i].bias))
+            i += 1
+        final_output = softmax(output_layer)
+        
+        if(check_prediction(final_output, target_number) == 1):
+            correct_number += 1
+        iteration += 1
+        accuracy = int((correct_number/iteration)*100)
+        print("Iteration: ", iteration, " Accuracy = ", accuracy, "%")
+        
+    
+    
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Elapsed time: {elapsed_time:.4f} seconds")
-    
-    
-    '''
-    Picture Generation
-    '''
-    '''
-    i = 0
-    while i < len(layer_2_feature_maps):        
-        picture_generator.matrix_to_bw_image(normalize(layer_2_feature_maps[i]), output_file=str(i) + ".png", scale_factor=10)
-        i += 1
-    '''
+    print("Elapsed Time: ", elapsed_time, " Accuracy: ", accuracy)
+
     
 
     
--- CNN code/file_loader.py
+++ CNN code/file_loader.py
@@ -4,10 +4,8 @@ Created on Tue Mar 11 17:09:48 2025
 
 @author: Alex
 """
-
 def read_file(filepath):
     with open(filepath, 'r') as file:
-        lines = file.readlines()  # Reads all lines into a list
-        [s.replace('\n', '') for s in lines]
-        return lines
+        lines = file.readlines()
+        return [float(line.strip()) for line in lines]
 
